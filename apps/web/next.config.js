71520ae89fcbe38d560a4dd68e626b61d634f22e
Dmitriy Popov <popov.dmitriiy@gmail.com>
2022-11-06 22:41:57 +0600

    fix: change dir name

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,7 @@ const withTM = require("next-transpile-modules")(["@mk/ui", "@mk/fetcher"])
 const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
+  distDir: "dist",
 })
 
 module.exports = nextConfig
